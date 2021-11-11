90d84fd87af4663e77346e6d8741e4f0789d57d6
dimisini <dimisini@gmail.com>
2021-11-11 16:20:16 +0300

    98% complete

---
+++ 092.js
@@ -0,0 +1,25 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+
+myMusic[1] = {
+  "artist": "Queen",
+  "title": "Bohemian Rapsody",
+  "release_year": 1978,
+  "formats": [
+    "CD",
+    "8T",
+    "LP"
+  ],
+  "platinum": true
+}
